7321629f0dba2a68dd0092f6f9bba92077eaba01
vaibhav-bhoir <bhoirvaibhav90@gmail.com>
2026-01-29 09:36:38 +0530

    feat: Add environment variables for LAMBDA_URL and API_KEY in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    LAMBDA_URL: process.env.LAMBDA_URL,
+    API_KEY: process.env.API_KEY,
+  },
 };
 
 export default nextConfig;
